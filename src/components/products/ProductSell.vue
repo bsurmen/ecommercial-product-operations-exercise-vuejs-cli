0c390d7dc22d347f6a685bb39b0ce77002c0b8cb
Burak Surmen <busurmen@gmail.com>
2020-10-07 15:29:53 +0300

    Added business logic and fixes to sale module.

--- src/components/products/ProductSell.vue
+++ src/components/products/ProductSell.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="container">
+    <div class="loading" :style="isLoading">
+      <div class="lds-ripple">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
     <div class="row">
       <div class="col-6 offset-3 pt-3 card mt-5 shadow">
         <div class="card-body">
@@ -7,52 +13,126 @@
           <hr />
           <div class="form-group">
             <label>Product Name</label>
-            <select class="form-control">
-              <option value="1">Product 1</option>
-              <option value="1">Product 2</option>
-              <option value="1">Product 3</option>
-              <option value="1">Product 4</option>
-              <option value="1">Product 5</option>
+            <select
+              class="form-control"
+              v-model="selectedProduct"
+              @change="productSelected"
+            >
+              <option selected disabled>Please choose a product...</option>
+              <option
+                :disabled="product.count == 0"
+                :value="product.key"
+                v-for="product in getProducts"
+              >
+                {{ product.title }}</option
+              >
             </select>
           </div>
-          <div class="card mb-2 border border-danger">
-            <div class="card-body">
-              <div class="row">
-                <div class="col-12 text-center">
-                  <div class="mb-3">
-                    <span class="badge badge-info">Stock : 4</span>
-                    <span class="badge badge-primary">Price : 100,5 $</span>
+          <transition name="fade" mode="out-in">
+            <div class="card mb-2 border border-danger" v-if="product !== null">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-12 text-center">
+                    <div class="mb-3">
+                      <span class="badge badge-info"
+                        >Stock : {{ product.count }}</span
+                      >
+                      <span class="badge badge-primary"
+                        >Price : {{ product.price | currency }}</span
+                      >
+                    </div>
+                    <p class="border border-warning p-2 text-secondary">
+                      {{ product.description }}
+                    </p>
                   </div>
-                  <p class="border border-warning p-2 text-secondary">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Assumenda debitis deleniti eos impedit iste numquam quos
-                    sit. Dignissimos, mollitia nemo officia reiciendis
-                    repellendus rerum velit. Eos libero magnam quas tempore!
-                  </p>
                 </div>
               </div>
             </div>
-          </div>
+          </transition>
           <div class="form-group">
             <label>Quantity</label>
             <input
               type="text"
+              v-model="product_count"
               class="form-control"
               placeholder="quantity"
             />
           </div>
           <hr />
-          <button class="btn btn-primary">Save</button>
+          <button @click="save" :disabled="saveEnabled" class="btn btn-primary">
+            Save
+          </button>
         </div>
       </div>
     </div>
   </div>
 </template>
-
 <script>
-export default {};
+import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      selectedProduct: null,
+      product: null,
+      product_count: null,
+      saveButtonClicked: false,
+    };
+  },
+  computed: {
+    ...mapGetters(["getProducts"]),
+    saveEnabled() {
+      if (this.selectedProduct !== null && this.product_count > 0) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    isLoading() {
+      if (this.saveButtonClicked) {
+        return {
+          display: "block",
+        };
+      } else {
+        return {
+          display: "none",
+        };
+      }
+    },
+  },
+  methods: {
+    productSelected() {
+      console.log(this.selectedProduct);
+      this.product = this.$store.getters.getProduct(this.selectedProduct)[0];
+    },
+    save() {
+      this.saveButtonClicked = true;
+      let product = {
+        key: this.selectedProduct,
+        count: this.product_count,
+      };
+      this.$store.dispatch("sellProduct", product);
+    },
+  },
+  beforeRouteLeave(to, from, next) {
+    if (
+      (this.selectedProduct !== null || this.product_count > 0) &&
+      !this.saveButtonClicked
+    ) {
+      if (
+        confirm(
+          "There are unsaved changes. Do you still want to leave the page?"
+        )
+      ) {
+        next();
+      } else {
+        next(false);
+      }
+    } else {
+      next();
+    }
+  },
+};
 </script>
-
 <style scoped>
 .border-danger {
   border-style: dashed !important;
